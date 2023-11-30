81fab125bb47bc4ed2e2ebe29bd08a3871dbcf46
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-29 20:17:55 -0500

    feat: add /loggerTest

--- src/app.js
+++ src/app.js
@@ -5,4 +5,10 @@ import usersRouter from './routers/users.router.js'
 const app = express()
 app.use('/api/users', usersRouter)
 
+app.get('/loggerTest', (req, res) => {
+    logger.debug('Test de logger Debug')
+    logger.http('Test de logger Http')
+    res.send('ok')
+})
+
 app.listen(8080, () => logger.info('Server Up'))
