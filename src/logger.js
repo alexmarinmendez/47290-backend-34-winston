552119fa927543a07a238708e9dbafe333c87d21
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-29 20:12:51 -0500

    feat: create loggers for each environment

--- src/logger.js
+++ src/logger.js
@@ -1,4 +1,6 @@
 import winston from 'winston'
+import dotenv from 'dotenv'
+dotenv.config()
 
 const customWinstonLevels = {
     levels: {
@@ -21,23 +23,35 @@ const customWinstonLevels = {
 
 winston.addColors(customWinstonLevels.colors)
 
-const logger = winston.createLogger({
-    levels: customWinstonLevels.levels,
-    transports: [
-        new winston.transports.Console({
-            level: 'fatal',
-            format: winston.format.combine(
-                winston.format.timestamp(),
-                winston.format.colorize(),
-                winston.format.simple()
-            )
-        }),
-        new  winston.transports.File({
-            filename: 'server.log',
-            level: 'info',
-            format: winston.format.json()
+const createLogger = env => {
+    if (env === 'PROD') {
+        return winston.createLogger({
+            levels: customWinstonLevels.levels,
+            transports: [
+                new  winston.transports.File({
+                    filename: 'server.log',
+                    level: 'fatal',
+                    format: winston.format.json()
+                })  
+            ]
         })
-    ]
-})
+    } else {
+        return winston.createLogger({
+            levels: customWinstonLevels.levels,
+            transports: [
+                new winston.transports.Console({
+                    level: 'fatal',
+                    format: winston.format.combine(
+                        winston.format.timestamp(),
+                        winston.format.colorize(),
+                        winston.format.simple()
+                    )
+                })
+            ]
+        })
+    }
+}
+
+const logger = createLogger(process.env.ENVIRONMENT)
 
 export default logger
