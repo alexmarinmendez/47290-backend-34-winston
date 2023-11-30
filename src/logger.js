4aa6ea843fc2b3e073790afe80f9128a89417ed6
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-29 20:01:47 -0500

    feat: add custom levels and colors

--- src/logger.js
+++ src/logger.js
@@ -1,9 +1,31 @@
 import winston from 'winston'
 
+const customWinstonLevels = {
+    levels: {
+        debug: 0,
+        http: 1,
+        info: 2,
+        warning: 3,
+        error: 4,
+        fatal: 5
+    },
+    colors: {
+        debug: 'white',
+        http: 'green',
+        info: 'blue',
+        warning: 'yellow',
+        error: 'magenta',
+        fatal: 'red'
+    }
+}
+
+winston.addColors(customWinstonLevels.colors)
+
 const logger = winston.createLogger({
+    levels: customWinstonLevels.levels,
     transports: [
         new winston.transports.Console({
-            level: 'silly',
+            level: 'fatal',
             format: winston.format.combine(
                 winston.format.timestamp(),
                 winston.format.colorize(),
