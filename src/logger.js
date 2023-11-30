2c8b78f024a0a5319b17ae46ea5016ddd714afec
Alex Marin Mendez <alexmarinmendez@gmail.com>
2023-11-29 19:53:16 -0500

    feat: create File transport

--- src/logger.js
+++ src/logger.js
@@ -3,6 +3,15 @@ import winston from 'winston'
 const logger = winston.createLogger({
     transports: [
         new winston.transports.Console({
+            level: 'silly',
+            format: winston.format.combine(
+                winston.format.timestamp(),
+                winston.format.colorize(),
+                winston.format.simple()
+            )
+        }),
+        new  winston.transports.File({
+            filename: 'server.log',
             level: 'info',
             format: winston.format.json()
         })
--- src/middlewares/auth.middleware.js
+++ src/middlewares/auth.middleware.js
@@ -3,7 +3,7 @@ import logger from "../logger.js"
 const auth = (req, res, next) => {
     const user = req.query.user
     if (user !== 'admin') {
-        logger.warn(`Usuario ${user} intentó acceder a la base de datos`)
+        logger.debug(`Usuario ${user} intentó acceder a la base de datos`)
         return res.send({ status: 'error', error: 'Not authorized' })
     }
     next()
